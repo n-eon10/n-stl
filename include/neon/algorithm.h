789805bd2faec1f185411d9f6114b41205871e07
Afam <78809835+n-eon10@users.noreply.github.com>
2026-01-27 22:39:29 +0000

    Add documentation to algorithm and iterator

--- include/neon/algorithm.h
+++ include/neon/algorithm.h
@@ -3,16 +3,39 @@
 
 namespace neon {
 
+/**
+ * @brief Returns the smaller of two values.
+ * @tparam T The type of values to compare.
+ * @param lhs The left-hand side value.
+ * @param rhs The right-hand side value.
+ * @return A const reference to the smaller value.
+ */
 template<class T>
 constexpr const T& min(const T& lhs, const T& rhs) {
     return (rhs < lhs) ? rhs : lhs;
 }
 
+/**
+ * @brief Returns the larger of two values.
+ * @tparam T The type of values to compare.
+ * @param lhs The left-hand side value.
+ * @param rhs The right-hand side value.
+ * @return A const reference to the larger value.
+ */
 template<class T>
 constexpr const T& max(const T& lhs, const T& rhs) {
     return (lhs > rhs) ? lhs : rhs;
 }
 
+/**
+ * @brief Finds the first element equal to the specified value.
+ * @tparam Iterator The type of iterator.
+ * @tparam T The type of value to search for.
+ * @param first The start of the range to search.
+ * @param last The end of the range to search.
+ * @param value The value to search for.
+ * @return An iterator to the first matching element, or last if no match is found.
+ */
 template<class Iterator, class T>
 Iterator find(Iterator first, Iterator last, const T& value) {
     for (; first != last; ++first) {
@@ -22,6 +45,15 @@ Iterator find(Iterator first, Iterator last, const T& value) {
     return last;
 }
 
+/**
+ * @brief Copies a range of elements to another range.
+ * @tparam InputIt The type of input iterator.
+ * @tparam OutputIt The type of output iterator.
+ * @param first The start of the range to copy.
+ * @param last The end of the range to copy.
+ * @param out The start of the destination range.
+ * @return An iterator to the element following the last copied element.
+ */
 template <class InputIt, class OutputIt>
 OutputIt copy(InputIt first, InputIt last, OutputIt out) {
     for (; first != last; ++first, ++out) {
@@ -30,6 +62,15 @@ OutputIt copy(InputIt first, InputIt last, OutputIt out) {
     return out;
 }
 
+/**
+ * @brief Moves a range of elements to another range.
+ * @tparam InputIt The type of input iterator.
+ * @tparam OutputIt The type of output iterator.
+ * @param first The start of the range to move.
+ * @param last The end of the range to move.
+ * @param out The start of the destination range.
+ * @return An iterator to the element following the last moved element.
+ */
 template <class InputIt, class OutputIt>
 OutputIt move(InputIt first, InputIt last, OutputIt out) {
     for (; first != last; ++first, ++out) {
@@ -38,6 +79,14 @@ OutputIt move(InputIt first, InputIt last, OutputIt out) {
     return out;
 }
 
+/**
+ * @brief Fills a range with the specified value.
+ * @tparam Iterator The type of iterator.
+ * @tparam T The type of value to fill with.
+ * @param first The start of the range to fill.
+ * @param last The end of the range to fill.
+ * @param value The value to fill with.
+ */
 template<class Iterator, class T>
 void fill(Iterator first, Iterator last, const T& value) {
     for (; first != last; ++first) {
@@ -45,6 +94,15 @@ void fill(Iterator first, Iterator last, const T& value) {
     }
 }
 
+/**
+ * @brief Checks if two ranges are equal.
+ * @tparam It1 The type of iterator for the first range.
+ * @tparam It2 The type of iterator for the second range.
+ * @param first1 The start of the first range.
+ * @param last1 The end of the first range.
+ * @param first2 The start of the second range.
+ * @return true if all elements in the ranges are equal, false otherwise.
+ */
 template <class It1, class It2>
 bool equal(It1 first1, It1 last1, It2 first2) {
   for (; first1 != last1; ++first1, ++first2) {
@@ -53,6 +111,16 @@ bool equal(It1 first1, It1 last1, It2 first2) {
   return true;
 }
 
+/**
+ * @brief Checks if the first range is lexicographically less than the second range.
+ * @tparam It1 The type of iterator for the first range.
+ * @tparam It2 The type of iterator for the second range.
+ * @param first1 The start of the first range.
+ * @param last1 The end of the first range.
+ * @param first2 The start of the second range.
+ * @param last2 The end of the second range.
+ * @return true if the first range is lexicographically less than the second, false otherwise.
+ */
 template <class It1, class It2>
 bool lexicographical_compare(It1 first1, It1 last1, It2 first2, It2 last2) {
   for (; first1 != last1 && first2 != last2; ++first1, ++first2) {
--- include/neon/iterator.h
+++ include/neon/iterator.h
@@ -2,35 +2,81 @@
 
 namespace neon{
 
+/**
+ * @brief Tag type for input iterators.
+ * Defines an iterator category for iterators that can only read values in a forward direction.
+ */
 struct input_iterator_tag {};
+
+/**
+ * @brief Tag type for forward iterators.
+ * Defines an iterator category for iterators that can read values in a forward direction multiple times.
+ */
 struct forward_iterator_tag : input_iterator_tag {};
+
+/**
+ * @brief Tag type for bidirectional iterators.
+ * Defines an iterator category for iterators that can read values in both forward and backward directions.
+ */
 struct bidirectional_iterator_tag : forward_iterator_tag {};
+
+/**
+ * @brief Tag type for random access iterators.
+ * Defines an iterator category for iterators that support random access in constant time.
+ */
 struct random_access_iterator_tag : bidirectional_iterator_tag {};
 
+/**
+ * @brief Provides type information about iterators.
+ * @tparam It The iterator type.
+ */
 template<class It>
 struct iterator_traits {
+    /// @brief The type used to represent distances between iterators.
     using difference_type = typename It::difference_type;
+    /// @brief The type of values accessed through the iterator.
     using value_type = typename It::value;
+    /// @brief The type of pointers to the iterated values.
     using pointer = typename It::pointer;
+    /// @brief The type of references to the iterated values.
     using reference = typename It::reference;
+    /// @brief The iterator category tag.
     using iterator_category = typename It::iterator_category;
 };
 
+/**
+ * @brief Specialisation of iterator_traits for raw pointers.
+ * @tparam T The type being pointed to.
+ */
 template <class T>
 struct iterator_traits<T*> {
+    /// @brief The type used to represent distances between pointers.
     using difference_type   = std::ptrdiff_t;
+    /// @brief The type of values accessed through the pointer.
     using value_type        = T;
+    /// @brief The type of the pointer itself.
     using pointer           = T*;
+    /// @brief The type of references to the pointed to values.
     using reference         = T&;
+    /// @brief Pointers are random access iterators.
     using iterator_category = random_access_iterator_tag;
 };
 
+/**
+ * @brief Specialisation of iterator_traits for const pointers.
+ * @tparam T The type being pointed to.
+ */
 template <class T>
 struct iterator_traits<const T*> {
+    /// @brief The type used to represent distances between const pointers.
     using difference_type   = std::ptrdiff_t;
+    /// @brief The type of values accessed through the const pointer.
     using value_type        = T;
+    /// @brief The type of the const pointer itself.
     using pointer           = const T*;
+    /// @brief The type of const references to the pointed-to values.
     using reference         = const T&;
+    /// @brief Const pointers are random access iterators.
     using iterator_category = random_access_iterator_tag;
 };
 
