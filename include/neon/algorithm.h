d27816253f5a6701558c5a65ae4ac12f61ba0461
Afam <78809835+n-eon10@users.noreply.github.com>
2026-01-27 20:57:46 +0000

    Add equal and lexicographical_compare algorithms

--- include/neon/algorithm.h
+++ include/neon/algorithm.h
@@ -45,4 +45,21 @@ void fill(Iterator first, Iterator last, const T& value) {
     }
 }
 
+template <class It1, class It2>
+bool equal(It1 first1, It1 last1, It2 first2) {
+  for (; first1 != last1; ++first1, ++first2) {
+    if (!(*first1 == *first2)) return false;
+  }
+  return true;
+}
+
+template <class It1, class It2>
+bool lexicographical_compare(It1 first1, It1 last1, It2 first2, It2 last2) {
+  for (; first1 != last1 && first2 != last2; ++first1, ++first2) {
+    if (*first1 < *first2) return true;
+    if (*first2 < *first1) return false;
+  }
+  return (first1 == last1) && (first2 != last2);
+}
+
 } // namespace neon
